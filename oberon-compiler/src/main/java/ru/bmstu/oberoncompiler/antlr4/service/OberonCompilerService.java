b1f06c5e5f0e3e2f5df8a4dc9039b24c2cdd558c
Bryanskaya <bryanskayakatya@yandex.ru>
2023-06-23 00:13:46 +0300

    REFACTOR: compiler service file

--- oberon-compiler/src/main/java/ru/bmstu/oberoncompiler/antlr4/service/OberonCompilerService.java
+++ oberon-compiler/src/main/java/ru/bmstu/oberoncompiler/antlr4/service/OberonCompilerService.java
@@ -7,7 +7,6 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
-import org.bytedeco.javacpp.*;
 import org.bytedeco.llvm.LLVM.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,8 +24,6 @@ import static ru.bmstu.oberoncompiler.antlr4.service.ShutdownListener.runCommand
 @Service
 public class OberonCompilerService {
     private OberonLexer lexer;
-    private LLVMBuilderRef builder;
-    private LLVMContextRef context;
     private LLVMModuleRef module;
 
     @Autowired
@@ -47,8 +44,6 @@ public class OberonCompilerService {
         walker.walk(new OberonWalker(parser), tree);
 
         module = (LLVMModuleRef) visitor.visitModule((OberonParser.ModuleContext) context);
-
-//        module = createModule();
 
         // Вывод LLVM-IR на консоль
         LLVMDumpModule(module);
@@ -72,37 +67,7 @@ public class OberonCompilerService {
         return new OberonParser(tokens);
     }
 
-    private LLVMModuleRef createModule() {
-        LLVMLinkInMCJIT();
-        LLVMInitializeNativeAsmPrinter();
-        LLVMInitializeNativeAsmParser();
-        LLVMInitializeNativeDisassembler();
-        LLVMInitializeNativeTarget();
-
-        context = LLVMContextCreate();
-        LLVMModuleRef module = LLVMModuleCreateWithNameInContext(appParams.moduleName, context);
-
-        LLVMTypeRef[] paramTypes = { LLVMInt32Type(), LLVMInt32Type() };
-        LLVMTypeRef functionType = LLVMFunctionType(LLVMInt32Type(), new PointerPointer<>(paramTypes), 2, 0);
-        LLVMValueRef function = LLVMAddFunction(module, "add", functionType);
-
-        // Создание базового блока
-        LLVMBasicBlockRef entry = LLVMAppendBasicBlock(function, "entry");
-        builder = LLVMCreateBuilder();
-        LLVMPositionBuilderAtEnd(builder, entry);
-
-        // Генерация инструкций
-        LLVMValueRef param1 = LLVMGetParam(function, 0);
-        LLVMValueRef param2 = LLVMGetParam(function, 1);
-        LLVMValueRef result = LLVMBuildAdd(builder, param1, param2, "result");
-        LLVMBuildRet(builder, result);
-
-        return module;
-    }
-
     private void freeResources() {
-        LLVMDisposeBuilder(builder);
         LLVMDisposeModule(module);
-        LLVMContextDispose(context);
     }
 }
